955ff7fa647e7f8220adb34e9aa8372e8f36e0dd
yiqiwang(王一棋) <yiqiwang@tencent.com>
2017-09-07 22:31:17 +0800

    [CHANGE] add log

--- WebViewJavascriptBridge/WKWebViewJavascriptBridge.h
+++ WebViewJavascriptBridge/WKWebViewJavascriptBridge.h
@@ -15,18 +15,41 @@
 #import "WebViewJavascriptBridgeBase.h"
 #import <WebKit/WebKit.h>
 
+
+/**
+ * ojbC API
+ */
 @interface WKWebViewJavascriptBridge : NSObject<WKNavigationDelegate, WebViewJavascriptBridgeBaseDelegate>
 
+/**
+ * create a javascript bridge for the given web view.
+ */
 + (instancetype)bridgeForWebView:(WKWebView*)webView;
 + (void)enableLogging;
 
+/**
+ * register a handler called "handlerName". The javascript can then call this handler with WebViewJavascriptBridge.callHandler("handlerName")
+ */
 - (void)registerHandler:(NSString*)handlerName handler:(WVJBHandler)handler;
 - (void)removeHandler:(NSString*)handlerName;
+
+/**
+ * Call the javascript handler called handlerName. If a responseCallback block is given the javascript handler can respond.
+ */
 - (void)callHandler:(NSString*)handlerName;
 - (void)callHandler:(NSString*)handlerName data:(id)data;
 - (void)callHandler:(NSString*)handlerName data:(id)data responseCallback:(WVJBResponseCallback)responseCallback;
+
 - (void)reset;
+
+/**
+ * Optionally, set a WKNavigationDelegate/UIWebViewDelegate if you need to respond to the web view's lifecycle events.
+ */
 - (void)setWebViewDelegate:(id)webViewDelegate;
+
+/**
+ * UNSAFE. Speed up bridge message passing by disabling the setTimeout safety check. It is only safe to disable this safety check if you do not call any of the javascript popup box functions (alert, confirm, and prompt). If you call any of these functions from the bridged javascript code, the app will hang.
+ */
 - (void)disableJavscriptAlertBoxSafetyTimeout;
 
 @end
